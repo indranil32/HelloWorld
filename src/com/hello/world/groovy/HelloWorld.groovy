07ea93fa66dbb34b926ad4536b7a92d5b5e816da
Indranil Majumder <indranil.majumder@quickplay.com>
2016-01-25 18:55:14 +0800

    stubs

---
+++ src/com/hello/world/groovy/HelloWorld.groovy
@@ -0,0 +1,9 @@
+package com.hello.world.groovy
+
+class HelloWorld {
+
+	static main(args) {
+		println "Hello World!!"
+	}
+
+}
---
+++ src/com/hello/world/java/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.hello.world.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
